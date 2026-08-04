efc78d0257b6b0ef533b267ef3cf1fd9cf98bd36
YoshrithMalhotra <malhotrayoshrith@gmail.com>
2026-08-04 19:45:40 +0530

    .

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"url-shortener/internal/handlers"
+)
+
 func main() {
-	
+
+	http.HandleFunc("/shorten", handlers.ShortenHandler)
+
+	log.Println("🚀 Server running on http://localhost:8080")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
